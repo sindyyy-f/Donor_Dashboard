72e427c47a024143dd76c6454ab8b8aa92267481
sindyyy-f <favila-favilas@students.trinitydc.edu>
2026-09-08 17:08:02 -0400

    Add reusable database connection

--- test_connection.py
+++ test_connection.py
@@ -1,17 +1,6 @@
-import os
-import mysql.connector
-from dotenv import load_dotenv
+from database import get_connection
 
-load_dotenv()
-
-connection = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT", "3306")),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
-)
-
+connection = get_connection()
 cursor = connection.cursor()
 
 cursor.execute("SELECT COUNT(*) FROM donors")
